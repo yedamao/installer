5d28eed32fae145cf2c200c0edd5d7acdc9f7b17
yedamo <logindaveye@gmail.com>
2021-11-26 18:35:59 +0800

    rename

--- me.sh
+++ me.sh
@@ -21,12 +21,16 @@ set -o nounset                              # Treat unset variables as an error
 
 export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;
 
-# install oh my zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 # dotfiles
-sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply $GITHUB_ACCOUNT
+sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply yedamao
 
 # setup vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+vim -c PlugInstall
+
+~/.vim/plugged/youcompleteme/install.sh --clang-completer --go-completer
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
