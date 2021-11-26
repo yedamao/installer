d27a7fc78446925ae4ab0dee0508f10e5a82ce04
yedamo <logindaveye@gmail.com>
2021-11-26 17:04:53 +0800

    first commit

---
+++ damao.sh
@@ -0,0 +1,28 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: damao.sh
+# 
+#         USAGE: ./damao.sh
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: yedamao,
+#  ORGANIZATION: 
+#       CREATED: 11/26/2021 16:20
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+export http_proxy=http://127.0.0.1:1087;export https_proxy=http://127.0.0.1:1087;
+
+# install oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# dotfiles
+sh -c "$(curl -fsLS git.io/chezmoi)" -- init --apply $GITHUB_ACCOUNT
---
+++ installer.sh
@@ -0,0 +1,40 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: installer.sh
+# 
+#         USAGE: sh installer.sh 
+# 
+#   DESCRIPTION:
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: yedamao, 
+#  ORGANIZATION: 
+#       CREATED: 11/26/2021 14:21
+#      REVISION:  ---
+#===============================================================================
+set -o nounset                              # Treat unset variables as an error
+
+# install pkg
+apt update && apt install -y vim zsh tmux shadowsocks-libev privoxy
+
+# setup ss-local
+cp ss-local.json /etc/shadowsocks-libev/
+cp shadowsocks-libev-local.service /etc/systemd/system/
+chmod 644 /etc/systemd/system/shadowsocks-libev-local.service
+systemctl start shadowsocks-libev-local.service
+systemctl enable shadowsocks-libev-local.service
+
+# setup privoxy convert sock5 to http
+cp privoxy.config /etc/privoxy/config
+systemctl restart privoxy.service
+
+# create user
+useradd $CREATE_USER_NAME -m -s /bin/zsh
+passwd $CREATE_USER_NAME
+adduser $CREATE_USER_NAME sudo
+
+su damao; cd;
