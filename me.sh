9191e7ea2ff6065e4b157a049acc18c3ba45fe2f
yedamo <logindaveye@gmail.com>
2021-12-02 22:09:36 +0800

    fix VERSION

--- me.sh
+++ me.sh
@@ -47,8 +47,8 @@ fi
 
 install_code_server() {
   VERSION=3.12.0
-  curl -fOL https://github.com/cdr/code-server/releases/download/v$VERSION/code-server_$VERSION_amd64.deb
-  sudo dpkg -i code-server_$VERSION_amd64.deb
+  curl -fOL https://github.com/cdr/code-server/releases/download/v${VERSION}/code-server_${VERSION}_amd64.deb
+  sudo dpkg -i code-server_${VERSION}_amd64.deb
   sudo systemctl enable --now code-server@$USER
 
   # change bind addr
