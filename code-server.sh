16f26e057b1d828206f86574aaf29108a992cf6a
yedamo <logindaveye@gmail.com>
2022-01-05 19:03:22 +0800

    Update code-server.sh

--- code-server.sh
+++ code-server.sh
@@ -7,8 +7,7 @@
 # 
 #   DESCRIPTION: setup code server
 # 
-#       OPTIONS: ---
-#  REQUIREMENTS: ---
+#       OPTIONS: --- #  REQUIREMENTS: ---
 #          BUGS: ---
 #         NOTES: ---
 #        AUTHOR: yedamao, 
@@ -17,28 +16,50 @@
 #      REVISION:  ---
 #===============================================================================
 
-set -o nounset                              # Treat unset variables as an error
-
 # Default settings
 VERSION=${CODE_SERVER_VERSION:-4.0.0}
 
-curl -fOL https://github.com/cdr/code-server/releases/download/v${VERSION}/code-server_${VERSION}_amd64.deb
-sudo -S dpkg -i code-server_${VERSION}_amd64.deb
-sudo systemctl enable --now code-server@$USER
-sudo systemctl restart code-server@$USER
+install() {
+  curl -fOL https://github.com/cdr/code-server/releases/download/v${VERSION}/code-server_${VERSION}_amd64.deb
+  sudo -S dpkg -i code-server_${VERSION}_amd64.deb
+  sudo systemctl enable --now code-server@$USER
+  sudo systemctl restart code-server@$USER
 
-# install caddy
-sudo -S apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/cfg/gpg/gpg.155B6D79CA56EA34.key' | sudo apt-key add -
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/cfg/setup/config.deb.txt?distro=debian&version=any-version' | sudo tee -a /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install caddy
+  # install caddy
+  sudo -S apt install -y debian-keyring debian-archive-keyring apt-transport-https
+  curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/cfg/gpg/gpg.155B6D79CA56EA34.key' | sudo apt-key add -
+  curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/cfg/setup/config.deb.txt?distro=debian&version=any-version' | sudo tee -a /etc/apt/sources.list.d/caddy-stable.list
+  sudo apt update
+  sudo apt install caddy
 
-# caddy config
-cat << EOF > Caddyfile
+  # caddy config
+  cat << EOF > Caddyfile
 $CODE_SERVER_DOMAIN_NAME
 reverse_proxy 127.0.0.1:8080
 EOF
 
-sudo cp Caddyfile /etc/caddy/Caddyfile
-sudo systemctl reload caddy
+  sudo cp Caddyfile /etc/caddy/Caddyfile
+  sudo systemctl reload caddy
+}
+
+uninstall() {
+  sudo systemctl stop caddy@service
+  sudo systemctl stop code-server@`whoami`.service
+  sudo apt remove --purge caddy code-server
+}
+
+main() {
+  if [ "$1" == "--uninstall" ]; then
+    uninstall
+  elif [ "$1" == "--install" ]; then
+    install
+  else
+    cat << EOF
+usage:
+--install install code server
+--uninstall uninstall code server
+EOF
+  fi
+}
+
+main "$@"
