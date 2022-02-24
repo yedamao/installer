0b850e0d3272accceea940d6397424f71046df3d
yedamo <logindaveye@gmail.com>
2022-02-24 04:14:36 +0000

    install.sh support yum

--- install.sh
+++ install.sh
@@ -18,10 +18,17 @@
 #===============================================================================
 set -e
 
-PKG_MANAGER=$( command -v yum || command -v apt ) || ( echo "Neither yum nor apt found" && exit )
+command -v yum >/dev/null 2>&1 && PKG_MANAGER="yum"
+command -v apt >/dev/null 2>&1 && PKG_MANAGER="apt"
+
+if [ "$PKG_MANAGER" = "" ]; then
+  echo "Neither yum nor apt found" && exit 1
+fi
+
+echo "PKG_MANAGER:" $PKG_MANAGER
 
 # install zsh
-$PKG_MANAGER update && $PKG_MANAGER install -y zsh
+$PKG_MANAGER update && $PKG_MANAGER install -y zsh curl
 
 # Default settings
 CREATE_USER_NAME=${CREATE_USER_NAME:-damao}
@@ -29,6 +36,16 @@ CREATE_USER_NAME=${CREATE_USER_NAME:-damao}
 # create user
 useradd $CREATE_USER_NAME -m -s /bin/zsh
 passwd $CREATE_USER_NAME
-adduser $CREATE_USER_NAME sudo
+
+# add USER to sudoers
+
+# ubuntu
+if [ "$PKG_MANAGER" = "apt" ]; then
+  adduser $CREATE_USER_NAME sudo
+fi
+# centos
+if [ "$PKG_MANAGER" = "yum" ]; then
+  usermod -aG wheel $CREATE_USER_NAME
+fi
 
 su -c "$(curl -fsSL https://raw.githubusercontent.com/yedamao/installer/main/me.sh)" $CREATE_USER_NAME
